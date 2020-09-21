ca787e6f6cbf695e6a0b86da39cd12a38ef6e599
Felix Sun <felix.sun.roc@gmail.com>
2020-09-21 18:18:33 +0800

    Add functions 1. open coco keypoints 2. batch generate and save anontated images

--- openposeImageKey/openposeImageFrame.cs
+++ openposeImageKey/openposeImageFrame.cs
@@ -9,12 +9,23 @@ namespace openposeImageKey
 {
     public class openposeImageFrame
     {
+        public enum keyPointModelType
+        {
+            body_25,
+            coco
+        }
+
         public string fileName = null;
         public List<openposeKeyPoint[]> bodys = null;
+        public keyPointModelType keyType;
 
-        public openposeImageFrame()
+        public openposeImageFrame(string keyPointType="body_25")
         {
             bodys = new List<openposeKeyPoint[]>();
+            if(keyPointType.ToUpper()=="COCO")
+            {
+                this.keyType = keyPointModelType.coco;
+            }
         }
 
         static public openposeImageFrame[] loadFromTextFile(string file)
@@ -63,6 +74,53 @@ namespace openposeImageKey
             return rtn.ToArray();
         }
 
+        static public openposeImageFrame[] loadFromTextFile_coco(string file)
+        {
+            List<openposeImageFrame> rtn = new List<openposeImageFrame>();
+            string line = "";
+            string recordStr = "";
+            bool inSection = false;
+
+
+            System.IO.StreamReader fr = new System.IO.StreamReader(@file);
+            try
+            {
+                while ((line = fr.ReadLine()) != null)
+                {
+                    if (line.Trim().Length < 1)
+                        continue;
+
+                    if (inSection)
+                    {
+                        if (line[0] == '>')
+                        {
+                            openposeImageFrame oif = openposeImageFrame.fromTextLine_coco(recordStr);
+                            if (oif != null)
+                                rtn.Add(oif);
+
+                            recordStr = line.Substring(1);
+                        }
+                        else
+                            recordStr += Environment.NewLine+line;
+                    }
+                    else if (line[0] == '>')
+                    {
+                        inSection = true;
+                        recordStr = line.Substring(1);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                fr.Close();
+                throw ex;
+            }
+
+            return rtn.ToArray();
+        }
+
+
         static public openposeImageFrame fromTextLine(string line)
         {
             openposeImageFrame rtn= new openposeImageFrame();
@@ -90,6 +148,23 @@ namespace openposeImageKey
             return rtn;
         }
 
+        static public openposeImageFrame fromTextLine_coco(string line)
+        {
+            openposeImageFrame rtn = new openposeImageFrame();
+            rtn.keyType = keyPointModelType.coco;
+
+            string[] lines = line.Split('\n');
+            rtn.fileName = lines[0].Trim();
+
+            for(int i=1; i<lines.Length; ++i)
+            {
+                string body = lines[i].Trim();
+                if (body.Length > 0)
+                    rtn.bodys.Add(bodyFromLine_coco(body));
+            }
+
+            return rtn;
+        }
         static public openposeKeyPoint[] bodyFromLine(string line)
         {
             List<openposeKeyPoint> points = new List<openposeKeyPoint>();
@@ -106,6 +181,17 @@ namespace openposeImageKey
 
             return points.ToArray();
         }
-        
+
+        static public openposeKeyPoint[] bodyFromLine_coco(string line)
+        {
+            List<openposeKeyPoint> points = new List<openposeKeyPoint>();
+            string[] lines = line.Split('|');
+            
+
+            foreach (string ps in lines)
+                points.Add(openposeKeyPoint.fromString2D_coco(ps.Trim()));
+
+            return points.ToArray();
+        }
     }
 }
