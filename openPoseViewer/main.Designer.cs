3bffe02bd15052f5dd5a109396bbb312871a6d86
Felix Sun <felix.sun.roc@gmail.com>
2020-09-21 22:11:16 +0800

    add main windows

---
+++ openPoseViewer/main.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace openPoseViewer
+{
+    partial class main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cbxImage = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.cbxType = new System.Windows.Forms.ComboBox();
+            this.btnBatch = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnBatch);
+            this.panel1.Controls.Add(this.cbxImage);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(624, 65);
+            this.panel1.TabIndex = 0;
+            // 
+            // cbxImage
+            // 
+            this.cbxImage.FormattingEnabled = true;
+            this.cbxImage.Location = new System.Drawing.Point(128, 35);
+            this.cbxImage.Name = "cbxImage";
+            this.cbxImage.Size = new System.Drawing.Size(259, 20);
+            this.cbxImage.TabIndex = 3;
+            this.cbxImage.SelectedIndexChanged += new System.EventHandler(this.cbxImage_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel6);
+            this.panel4.Controls.Add(this.panel7);
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(624, 32);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.btnLoad);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(128, 32);
+            this.panel5.TabIndex = 1;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(3, 3);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(122, 26);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load keypoint file...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 683);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(624, 38);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.picMain);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 65);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(624, 618);
+            this.panel3.TabIndex = 2;
+            // 
+            // picMain
+            // 
+            this.picMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMain.Location = new System.Drawing.Point(0, 0);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(624, 618);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picMain.TabIndex = 0;
+            this.picMain.TabStop = false;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.cbxType);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel7.Location = new System.Drawing.Point(498, 0);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(126, 32);
+            this.panel7.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.lblFile);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(128, 0);
+            this.panel6.Name = "panel6";
+            this.panel6.Padding = new System.Windows.Forms.Padding(3);
+            this.panel6.Size = new System.Drawing.Size(370, 32);
+            this.panel6.TabIndex = 4;
+            // 
+            // lblFile
+            // 
+            this.lblFile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblFile.Location = new System.Drawing.Point(3, 3);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(364, 26);
+            this.lblFile.TabIndex = 0;
+            this.lblFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cbxType
+            // 
+            this.cbxType.FormattingEnabled = true;
+            this.cbxType.Items.AddRange(new object[] {
+            "Body_25",
+            "coco"});
+            this.cbxType.Location = new System.Drawing.Point(3, 7);
+            this.cbxType.Name = "cbxType";
+            this.cbxType.Size = new System.Drawing.Size(121, 20);
+            this.cbxType.TabIndex = 0;
+            // 
+            // btnBatch
+            // 
+            this.btnBatch.Location = new System.Drawing.Point(393, 35);
+            this.btnBatch.Name = "btnBatch";
+            this.btnBatch.Size = new System.Drawing.Size(122, 20);
+            this.btnBatch.TabIndex = 4;
+            this.btnBatch.Text = "Batch export...";
+            this.btnBatch.UseVisualStyleBackColor = true;
+            this.btnBatch.Click += new System.EventHandler(this.btnBatch_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 721);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(640, 760);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image viewer for openpose ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.main_FormClosing);
+            this.Load += new System.EventHandler(this.main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.ComboBox cbxImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.ComboBox cbxType;
+        private System.Windows.Forms.Button btnBatch;
+    }
+}
+
---
+++ openPoseViewer/main.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using openposeImageKey;
+using System.Drawing.Imaging;
+
+namespace openPoseViewer
+{
+    public partial class main : Form
+    {
+        private string path = "";
+        private string fileName = "";
+        private openposeImageFrame[] imageFrames = null;
+        private int imageIndex;
+
+        public main()
+        {
+            InitializeComponent();
+
+            this.path = Application.StartupPath;
+            this.imageIndex = 0;
+            this.cbxType.SelectedIndex = 0;
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Close openpose viewer?", "Openpose viewer", MessageBoxButtons.YesNo) != DialogResult.Yes)
+                e.Cancel = true;
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Load open pose key-point file";
+            ofd.InitialDirectory = this.path;
+            ofd.Filter = "Text file|*.txt|All files|*.*";
+            if(ofd.ShowDialog()== DialogResult.OK)
+            {
+                this.path = Path.GetDirectoryName(ofd.FileName);
+                this.fileName = Path.GetFileNameWithoutExtension(ofd.FileName);
+                lblFile.Text = "(" + this.fileName + ") " + this.path;
+
+                try
+                {
+                    if(cbxType.SelectedItem.ToString()=="coco")
+                    {
+                        this.imageFrames = openposeImageFrame.loadFromTextFile_coco(ofd.FileName);
+                    }
+                    else
+                        this.imageFrames = openposeImageFrame.loadFromTextFile(ofd.FileName);
+
+                    this.imageIndex = 0;
+                }
+                catch (Exception ex)
+                {
+                    imageFrames = null;
+                    MessageBox.Show("Failed to reading openpose keypoint file");
+                    return;
+                }
+
+                cbxImage.Items.Clear();
+                if (this.imageFrames == null || this.imageFrames.Length < 1)
+                    return;
+
+                foreach(openposeImageFrame f in this.imageFrames)
+                {
+                    try
+                    {
+                        if (File.Exists(this.path + "\\" + f.fileName))
+                            cbxImage.Items.Add(f.fileName);
+                    }
+                    catch { }
+                }
+
+                if(cbxImage.Items.Count>0)
+                {
+                    cbxImage.SelectedIndex = 0;
+                    cbxImage.Focus();
+                }
+            }
+        }
+
+        private void cbxImage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox c = (ComboBox)sender;
+            foreach (openposeImageFrame f in this.imageFrames)
+            {
+                if (f.fileName == c.SelectedItem.ToString())
+                {
+                    this.picMain.Image = Image.FromFile(this.path + "\\" + f.fileName);
+                    foreach(openposeKeyPoint[] kp in f.bodys)
+                    {
+                        if(cbxType.SelectedItem.ToString()=="coco")
+                        {
+                            imageLabeler.labelCoco(this.picMain.Image, kp);
+                        }    
+                        else
+                            imageLabeler.labelBody25(this.picMain.Image, kp);
+                    }
+                    
+                }
+            }
+        }
+
+        private void btnBatch_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog sfd = new FolderBrowserDialog();
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Select batch folder to save results";
+
+            ofd.InitialDirectory = this.path;
+            ofd.FileName = "selected file will be ignored";
+            ofd.CheckFileExists = false;
+            ofd.CheckPathExists = true;
+            ofd.ValidateNames = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string savePath = Path.GetDirectoryName(ofd.FileName);
+                foreach (openposeImageFrame f in this.imageFrames)
+                {
+                    try
+                    {
+                        //generate exist images only
+                        string pathName = this.path + "\\" + f.fileName;
+                        if (File.Exists(pathName))
+                        {
+                            Image im = Image.FromFile(pathName);
+                            foreach (openposeKeyPoint[] kp in f.bodys)
+                            {
+                                if (cbxType.SelectedItem.ToString() == "coco")
+                                {
+                                    imageLabeler.labelCoco(im, kp);
+                                }
+                                else
+                                    imageLabeler.labelBody25(im, kp);
+                            }
+                            string saveName = savePath + "\\" + Path.GetFileNameWithoutExtension(f.fileName)+"_gen.png";
+                            if(File.Exists(saveName))
+                            {
+                                MessageBox.Show(saveName + " is exist! abort batch process");
+                                return;
+                            }
+                            im.Save(saveName, ImageFormat.Png);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Failed to generate anotated image"+Environment.NewLine+ex.Message);
+                        return;
+                    }
+                }
+                MessageBox.Show("Anontation successful");
+            }
+        }
+    }
+}
